6a47bf8fcca77354f9faf625eb92aa94294732dc
xstephaniexsouzax <stephaniesis024@gmail.com>
2022-10-25 17:27:53 -0300

    first commit

--- aula.js
+++ aula.js
@@ -273,4 +273,176 @@ if (tipoDePagamento === 1) {
     console.log(aplicarJuros(precoEtiqueta, 10));
 }
 
+---- Objetos e Classes ---- 
+
+const Stephanie ={
+    nome: 'stephanie',
+    idade: 23,
+}
+
+console.log(Stephanie.nome);
+console.log(Stephanie.idade);
+console.log(Stephanie);
+
+Stephanie.altura= 1.60;
+console.log(Stephanie);
+
+obs : alt+96= `
+
+
+const Pessoa = {
+    nome: 'stephanie',
+    idade: 23,
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+};
+
+Pessoa.nome= 'joabe';
+Pessoa.idade= 18;
+
+Pessoa.descrever();
+
+
+class Pessoa{
+    nome;
+    idade;
+    descrever () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);       
+    }
+}
+
+const jefte = new Pessoa();
+    jefte.nome = 'jefte-feio';
+    jefte.idade= 21;
+
+const stephanie= new Pessoa();
+    stephanie.nome= 'stephanie-bonita';
+    stephanie.idade=23;
+
+console.log(jefte);
+console.log(stephanie);
+
+--- construtor-class ---- 
+
+class Pessoa{
+    nome;
+    idade;
+    constructor (nome,idade) {
+        this.nome= nome;
+        this.idade=idade;
+        this.anoDeNascimento = 2022- idade;       
+    }
+    descrever(){
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);  
+    }
+}
+
+const stephanie= new Pessoa('stephanie-bonita', 23);
+const jefte= new Pessoa('jefte-feio', 20);
+const joabe= new Pessoa('joabe-insuportavel', 17);
+
+
+console.log(jefte);
+console.log(stephanie);
+console.log(joabe);
+
+
+
+class Pessoa {
+    nome;
+    idade;
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+
+function compararIdades(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
+const stephanie = new Pessoa('stephanie-bonita', 23);
+const jefte = new Pessoa('jefte-feio', 40);
+
+compararIdades(stephanie, jefte);
+
+---- Exercicios ----
+1) Crie uma classe para representar carros. 
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+gasto em reais para realizar este percurso.
+
+
+class carros {
+    marca;
+    cor;
+    gastoMedioPorKm;
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.cor= cor;
+        this.marca= marca;
+        this.gastoMedioPorKm = gastoMedioPorKm 
+    }
+    calcularPercurso (distanciaEmKm,precoDoCombustivel){
+        return distanciaEmKm* this.gastoMedioPorKm * precoDoCombustivel;
+    }
+   
+}
+
+const carro1= new carros ('Aston Martin','azul', 1/12);
+console.log(carro1.calcularPercurso(70,7));
+
+const palio = new carros ('fiat ', 'marron', 1/10);
+console.log(palio.calcularPercurso(60,7))
+
+2) Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;
+
+
+class Pessoa {
+    nome;
+    peso;
+    altura;
+    constructor(nome, peso, altura) {
+        this.nome= nome;
+        this.peso= peso;
+        this.altura= altura;
+    }
+    calcularImc (peso,altura){
+        return this.peso / (this.altura* this.altura);
+    } 
+    classificacaoImc(){
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return 'Abaixo do peso';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'Peso normal';
+        } else if (imc >= 25 && imc < 30) {
+            return 'Acima do peso';
+        } else if (imc >= 30 && imc < 50) {
+            return 'Obeso';
+        } else {
+            return 'Obesidade Grave';
+        }
+    } 
+}
+
+const jose = new Pessoa ('jose', 70, 1.75);
+console.log(jose.classificacaoImc());
+
+const marcos = new Pessoa ('marcos', 60, 1.85);
+console.log(marcos.classificacaoImc());
+
 */
