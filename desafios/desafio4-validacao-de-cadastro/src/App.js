86e22d5b4ca8db99ee5d1b17498bd7293394fb50
xstephaniexsouzax <stephaniesis024@gmail.com>
2022-12-07 17:42:17 -0300

    first commit

---
+++ desafios/desafio4-validacao-de-cadastro/src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+
+import React, { useState } from "react";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Your state values: \n 
+            email: ${email} \n 
+            password: ${password} \n 
+            You can replace this alert with your process`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:  </label> 
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          onChange={handleEmailChange}
+          value={email}
+        />
+      </div>
+      <div>
+        <label>Senha:  </label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+      </div>
+      <button type="submit" disabled={!email || !password}>
+        Login
+      </button>
+    </form>
+  );
+}
+
+
+
+export default App;
